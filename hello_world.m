5af91b97b999057bd003ddde9e132ca059fa1a83
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-10-27 21:35:27 +0800

    Update hello_world.m

--- hello_world.m
+++ hello_world.m
@@ -1,8 +1,23 @@
-reply = input('Hello World? Y/N [Y]', 's');
+close all
+clear
+% reply = input('Hello World? Y/N [Y]', 's');
 
-if isempty(reply)
-    reply = 'Y';
+% if isempty(reply)
+%     reply = 'Y';
 
-reply2 = input('What is your name?', 's')
-fprintf('The area is %8.5f\n', pi)
-end
+%     reply2 = input('What is your name?', 's')
+%     fprintf('The area is %8.5f\n', pi)
+
+% end
+
+x = rand(10, 10);
+y = x(1:2:end, 1:2:end);
+
+% plot(y)
+plot(y)
+figure
+plot(3, 3, '*')
+
+F = getframe(2);
+% close(F)
+% close(1)
