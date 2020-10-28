9b4288b905f2d0ce08864ab310005f886a3f1388
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-10-28 11:16:38 +0800

    Update plot_potential.m

--- experiment_1/plot_potential.m
+++ experiment_1/plot_potential.m
@@ -6,13 +6,11 @@ function plot_potential(V)
     %      the first indicates the x-coordinate of the meshgrid
     %      the second indicates the y-coordinate of the meshgrid
     %      the third indicates the potential at each points in the range
-    %   xm - the range of the field in x direction
-    %   ym - the range of the field in y direction
 
     figure
-    mesh(V{2}, V{3}, V{1});
+    mesh(V{2}, V{3}, V{1})
 
-    % The title for the plot(Note that all symbols should be half-angled English characters )
+    % The title for the plot(Note that all symbols should be half-angled English characters)
     title("The plot of electric potential distribution of point charge(s) in the vacuum");
     % label the x axis
     xlabel('X axis (unit: m)', 'fontname', 'Times New Roman');
